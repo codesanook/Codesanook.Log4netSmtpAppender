4b20c87a73149940b9d07079e7793751a072c568
aaronamm <theeranitp@gmail.com>
2019-07-13 12:38:25 +0700

    Update AssemblyTitle

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,10 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeSanook.Log4netSmtpAppender")]
+[assembly: AssemblyTitle("Codesanook.Log4netSmtpAppender")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Orchard")]
